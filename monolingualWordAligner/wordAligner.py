f7fa80596c3324223ec3a1fd5f17783cd890abac
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-10-22 17:12:23 +0200

    align_namedEntities and learn_NamedEntities functions added

--- monolingualWordAligner/wordAligner.py
+++ monolingualWordAligner/wordAligner.py
@@ -4,32 +4,18 @@ from nltkUtil import *
 class Aligner:
 
 	def __init__(self):
-		pass
+		self.text_nor = Text_processing()
 
 	def align_sentences(self,sentence1,sentence2):
 
-		text_nor = Text_processing()
-		
-		sentence1ParseResult = text_nor.parser(sentence1)
+		sentence1ParseResult = self.text_nor.parser(sentence1)
 		# print "sentence1 parse ", sentence1ParseResult 
 		# print ""
-		sentence2ParseResult = text_nor.parser(sentence2)
+		sentence2ParseResult = self.text_nor.parser(sentence2)
 
-		# sentence1Lemmas = text_nor.get_lemma(sentence1ParseResult)
-		# print "lemmas ", sentence1Lemmas
-		# print ""
-		# print "sentenceLem"
-		# sentence2Lemmas = text_nor.get_lemma(sentence2ParseResult)
-		# print "sentence2 ", sentence2Lemmas
-
-		# sentence1PosTags = text_nor.find_posTags(sentence1ParseResult)
-		# print "pos tag ", sentence1PosTags
-
-		# sentence2PosTags = text_nor.find_posTags(sentence2ParseResult)
-		# print "pos tag2 ", sentence2PosTags
-
-		sentence1LemmasAndPosTags = text_nor.combine_lemmaAndPosTags(sentence1ParseResult)
-		sentence2LemmasAndPosTags = text_nor.combine_lemmaAndPosTags(sentence2ParseResult)
+		sentence1LemmasAndPosTags = self.text_nor.combine_lemmaAndPosTags(sentence1ParseResult)
+		# print "sentce1 ", sentence1LemmasAndPosTags
+		sentence2LemmasAndPosTags = self.text_nor.combine_lemmaAndPosTags(sentence2ParseResult)
 
 		self.sourceWordIndices = [i+1 for i in xrange(len(sentence1LemmasAndPosTags))]
 		self.targetWordIndices = [i+1 for i in xrange(len(sentence2LemmasAndPosTags))]
@@ -69,9 +55,11 @@ class Aligner:
 
 		# align the punctuations
 		alignments, srcWordAlreadyAligned, tarWordAlreadyAligned = self.align_punctuations(self.sourceWords,self.targetWords, alignments, srcWordAlreadyAligned, tarWordAlreadyAligned,sourceSent,targetSent)
-		print "alignments ", alignments
-		print "source Wrod Already Aligned ", srcWordAlreadyAligned
-		print "target word Already Aligned ", tarWordAlreadyAligned
+		# print "alignments ", alignments
+		# print "source Wrod Already Aligned ", srcWordAlreadyAligned
+		# print "target word Already Aligned ", tarWordAlreadyAligned
+
+		neAlignments = self.align_namedEntities(sourceSent, targetSent, sourceParseResult, targetParseResult, alignments)
 
 
 		'''
@@ -106,6 +94,70 @@ class Aligner:
 
 		return alignments, srcWordAlreadyAligned, tarWordAlreadyAligned
 
+
+	def align_namedEntities(self, sourceSent, targetSent, sourceParseResult, targetParseResult, alignments):
+		
+		
+		sourceNE = self.text_nor.get_ner(sourceParseResult)
+		targetNE = self.text_nor.get_ner(targetParseResult)
+		print "Before source NR ", sourceNE
+		print "before target NE ", targetNE
+
+		sourceNE = self.learn_NamedEntities(sourceSent, sourceNE, targetNE)
+		targetNE = self.learn_NamedEntities(targetSent, targetNE, sourceNE)
+		print "After source Learn Entities ", sourceNE
+		print "After target Learn Entities ", targetNE
+
+	'''
+	Input: sentParam is list of:
+	 	[[character begin offset, character end offset], word index, word, lemma, pos tag]
+	 LearnNE, KnownNE is list of:
+		[[[['charBegin', 'charEnd'], ['charBegin', 'charEnd']], [wordIndex1, wordIndex2], ['United', 'States'], 'LOCATION']]
+	LearnNE(e.g we want to find NE tags in sent1) determine unknown NE Tags from knownNE(e.g known NE tags in sent2) 
+	Returns:  
+	'''
+	def learn_NamedEntities(self,SentParam, LearnNE, knownNE):
+		
+		for i in SentParam:
+			alreadyIncluded = False
+			for j in LearnNE:
+				# check if word Index of source word is present in sourceNE(already has NE)
+				if i[1] in j[1]:
+					# print "matched ", i[1], j[1]
+					alreadyIncluded = True
+					break
+			'''
+			If sourceWord is already included or i[2](word length) is > 0 and 
+			 firstLetterOfWord is not upper(No Acronym or not any name)
+			 Then do nothing(do not append list)
+			'''
+			if alreadyIncluded or (len(i[2]) > 0 and not i[2][0].isupper()):
+				continue
+
+			#If sourceWord does not have any Named Entity learn from targetSent
+
+			for k in knownNE:
+				#check if sourceword(i[2]) is present in k[2](target Word)
+				
+				if i[2] in k[2]:
+					#construct new item([ [charbegin,charEnd], [sourceWordIndex], [sourceWord], [targetWordNE] ])
+					# we replace NE of sourceWord with NE of targetWord 
+					newItem = [[i[0]], [i[1]], [i[2]], k[3]]
+					print "matched"
+					partOfABiggerName = False
+					for p in xrange(len(LearnNE)):
+						if LearnNE[p][1][len(LearnNE[p][1])-1] == newItem[1][0] - 1:
+							LearnNE[p][0].append(newItem[0][0])
+							LearnNE[p][1].append(newItem[1][0])
+							LearnNE[p][2].append(newItem[2][0])
+							partOfABiggerName = True
+					if not partOfABiggerName:
+						LearnNE.append(newItem)
+
+				elif self.text_nor.is_Acronym(i[2], k[2]) and [[i[0]], [i[1]], [i[2]], k[3]] not in LearnNE:
+					LearnNE.append([[i[0]], [i[1]], [i[2]], k[3]])
+
+		return LearnNE
 
 
 		
