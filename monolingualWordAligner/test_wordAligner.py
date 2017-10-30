ec49172e3c9e928bb45721606e4022843f7832b6
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-10-30 21:07:43 +0100

    test_wordAligner move in tests directory

--- monolingualWordAligner/test_wordAligner.py
+++
@@ -1,7 +0,0 @@
-from wordAligner import *
-
-def test_align_sentences():
-	aligner = Aligner()
-	result = aligner.align_sentences("UAE is one of biggest country in the world.","Many people are living in United Arab Emirates")
-	assert result == [['UAE', 'United'], ['UAE', 'Arab'], ['UAE', 'Emirates']]
-	
