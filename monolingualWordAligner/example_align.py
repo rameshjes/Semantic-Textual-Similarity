e906b4a3e825986fc8a27999470d78a0735d5ba4
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-10-30 22:39:33 +0100

    example changed

--- monolingualWordAligner/example_align.py
+++ monolingualWordAligner/example_align.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 from wordAligner import *
 
-sentence1 = "United States is one of biggest country in the world. Four people are dead from a collisions."
-sentence2 = "Second sentence need to be in written form."
-# sentence1 = "United State of America is one of biggest country in the world"
-print "sentence1 =",sentence1
-print "sentence2 =",sentence2
+sentence1 = "UAE is one of biggest country in the world."
+sentence2 = "Many people are living in United Arab Emirates"
 
-# aligner(sentence1,sentence2)
+print "sentence1 = ", sentence1
+print "sentence2 = ", sentence2
 
 if __name__ == '__main__':
+
 	processing = Aligner()
-	processing.align_sentences(sentence1,sentence2)
+	print "Words alignment ", processing.align_sentences(sentence1,sentence2)
