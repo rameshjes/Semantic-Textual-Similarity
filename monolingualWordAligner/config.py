c0c7bc901aa6a50a46dea8c1555f1de0475678be
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-11-05 00:12:10 +0100

    Snowball stemmer import from nltk

--- monolingualWordAligner/config.py
+++ monolingualWordAligner/config.py
@@ -5,8 +5,8 @@ theta1 = 0.9
 import nltk
 from nltk.corpus import stopwords
 
-# from nltk import SnowballStemmer
-# stemmer = SnowballStemmer('english')
+from nltk import SnowballStemmer
+stemmer = SnowballStemmer('english')
 
 punctuations = ['(','-lrb-','.',',','-','?','!',';','_',':','{','}','[','/',']','...','"','\'',')', '-rrb-']
 stopwords = stopwords.words('english')
