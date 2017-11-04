fb048d1b6d2bec4e43b227830d0200134b4bb296
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-11-04 17:14:35 +0100

    example for align hyphen words added

--- monolingualWordAligner/example_align.py
+++ monolingualWordAligner/example_align.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 from wordAligner import *
 
-sentence1 = "UAE is one of biggest country in the world."
-sentence2 = "Many people are living in United Arab Emirates"
+sentence1 = "UAE is really well-designed country in  the world. US is powerful country"
+sentence2 = "Really well designed is United Arab Emirates. But United States is more populated"
 
 print "sentence1 = ", sentence1
 print "sentence2 = ", sentence2
@@ -11,4 +11,4 @@ print "sentence2 = ", sentence2
 if __name__ == '__main__':
 
 	processing = Aligner()
-	print "Words alignment ", processing.align_sentences(sentence1,sentence2)
+	processing.align_sentences(sentence1,sentence2)
