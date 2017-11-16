4e6bb2b59341268bfcd0b92b0195fd684ab18225
Ramesh Kumar <rameshjesswani@yahoo.com>
2017-11-16 18:58:27 +0100

    general example for all modules added

--- monolingualWordAligner/example_align.py
+++ monolingualWordAligner/example_align.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 from wordAligner import *
 
-sentence1 = "I really do care. The woman is quite pretty"
-sentence2 = "He is doing really well. She looks quite beautiful"
+sentence1 = "I heared some noise. We are looking for a dog-friendly hotel. I am driving since few days. Four people died in accident. Well, United Arab Emirates is one of powerful country"
+sentence2 = "She use to drive on weekends. Seven men are dead due to collisions. Is this hotel dog friendly? I use to hear songs. UAE is well-designed country"
 
 print "sentence1 = ", sentence1
 print "sentence2 = ", sentence2
