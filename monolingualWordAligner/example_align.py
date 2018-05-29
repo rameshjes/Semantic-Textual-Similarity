4fb9db673b8c642f16de1974d7ddb46206d257ee
Ramesh Kumar <rameshjesswani@yahoo.com>
2018-05-29 10:33:21 +0200

    updated example and user input added

--- monolingualWordAligner/example_align.py
+++ monolingualWordAligner/example_align.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from wordAligner import *
+import sys
 
-sentence1 = "I heared some noise. We are looking for a dog-friendly hotel. I am driving since few days. Four people died in accident. Well, United Arab Emirates is one of powerful country"
-sentence2 = "She use to drive on weekends. Seven men are dead due to collisions. Is this hotel dog friendly? I use to hear songs. UAE is well-designed country"
+sentence1 = "Four people died in accident. Well, United Arab Emirates is one of powerful country"
+sentence2 = "Seven men are dead due to collisions."
 
 print "sentence1 = ", sentence1
 print "sentence2 = ", sentence2
 
 if __name__ == '__main__':
 
-	processing = Aligner()
+	flag = sys.argv[1]
+
+	processing = Aligner(flag)
 	processing.align_sentences(sentence1,sentence2)
